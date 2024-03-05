e1d31b2982944e19063adaed2032be06d0cec4f2
iosmitDev <Mitesh.imply@gmail.com>
2024-03-05 16:21:23 +0530

    initial commit

---
+++ NewTestProjectSVN/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  NewTestProjectSVN
+//
+//  Created by MiteshKumar Patel on 05/03/24.
+//
+
+import Foundation
---
+++ NewTestProjectSVN/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewTestProjectSVN
+//
+//  Created by MiteshKumar Patel on 05/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("hello Mitesh")
+    }
+
+
+}
+
